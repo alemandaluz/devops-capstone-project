e14c627e5a44175518eab4e5fe9b129f899b0fcc
alemandaluz <miguel_pcm@yahoo.co.uk>
2025-12-17 23:20:44 +0100

    Update __init__.py

--- service/__init__.py
+++ service/__init__.py
@@ -12,6 +12,7 @@ from service.common import log_handlers
 # Create Flask application
 app = Flask(__name__)
 app.config.from_object(config)
+talisman = Talisman(app)
 
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
